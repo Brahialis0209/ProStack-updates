7a9992494c4138d32445fa736c5ba41e59ebf5a9
Brahialis0209 <rafantem@mail.ru>
2021-03-11 13:20:05 +0300

    add input parametrs and work with mulitimage

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include "opencv2/core/utility.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <stdio.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+string file_name_parser(const string &str) {
+	string newStr;
+	char a = '\'';
+	for (size_t i = 0; i < str.size(); i++)
+		if (str[i] != a)
+			newStr.append(1, str[i]);
+	return newStr;
+}
+
+
+class Param_line 
+{
+public:
+
+	bool param_parser(int argc, const char** argv) {
+		int k1, k2;
+		int sigma;
+		double edgeThresh1;
+		double edgeThresh2;
+		if (argc == 0) return false;
+		k1 = (int)(argv[2][0]) - 48;
+		k2 = (int)(argv[2][2]) - 48;
+		sigma = stoi(argv[3]);
+		edgeThresh1 = stod(argv[4]);
+		edgeThresh2 = stod(argv[5]);
+		this->k1 = k1;
+		this->k2 = k2;
+		this->sigma = sigma;
+		this->edgeThresh1 = edgeThresh1;
+		this->edgeThresh2 = edgeThresh2;
+		return true;
+	}
+	int k1, k2;
+	int sigma;
+	double edgeThresh1;
+	double edgeThresh2;
+};
+
+
+
+
+Mat canny_work(Mat & image, Param_line & canny_params) {
+	Mat cedge, gray, blur_gaus, gray_res;
+	cedge.create(image.size(), image.type());
+	GaussianBlur(image, blur_gaus, Size(canny_params.k1,canny_params.k2), canny_params.sigma);
+	Canny(blur_gaus, cedge, canny_params.edgeThresh1, canny_params.edgeThresh2);
+	return cedge;
+}
+
+
+int main(int argc, const char** argv)
+{
+	string file_name_params[2];
+	vector<Mat> mats;
+	vector<Mat> canny_mats;
+	Param_line canny_params;
+	Mat image;
+	string filename;
+	string out_name;
+	Mat result_mat;
+	file_name_params[0] = argv[argc - 2];
+	file_name_params[1] = argv[argc - 1];
+	filename = file_name_parser(file_name_params[0]);
+	out_name = file_name_parser(file_name_params[1]);
+	if (!canny_params.param_parser(argc, argv)) {
+		printf("Cannot read image file: %s\n", filename.c_str());
+		return -1;
+	}
+	bool res = imreadmulti(filename,mats);
+	if (!res)
+	{
+		printf("Cannot read image file: %s\n", filename.c_str());
+		return -1;
+	}
+	for (Mat part : mats) {
+		//imshow("display window", part);
+		//int k = waitKey(0);
+		result_mat = canny_work(part, canny_params);
+		//imshow("display window", result_mat);
+		//k = waitKey(0);
+		canny_mats.push_back(result_mat);
+	}
+	imwritemulti(out_name, canny_mats);
+	return 0;
+}
